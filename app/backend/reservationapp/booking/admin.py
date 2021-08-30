88e99ab77411cb6d77690a54e180cdbf8e315bd2
wojtiwojt <wojti.wojt91@gmail.com>
2021-08-30 17:15:22 +0200

    model registration to admin panel

--- app/backend/reservationapp/booking/admin.py
+++ app/backend/reservationapp/booking/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Reservation
 
-# Register your models here.
+admin.site.register(Reservation)
+admin.site.site_header = "Reservation app - admin panel"
