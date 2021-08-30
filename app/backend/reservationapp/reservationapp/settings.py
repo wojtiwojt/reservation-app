4d446118bb70242ba4397a5fad25744fcedfdaaa
wojtiwojt <wojti.wojt91@gmail.com>
2021-08-30 17:10:36 +0200

    settings and models

--- app/backend/reservationapp/reservationapp/settings.py
+++ app/backend/reservationapp/reservationapp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
+    'booking'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
